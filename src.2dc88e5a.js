7159acfd51d414c8ca0a1b9bdc51efc60dfd5575
SeregaDS <95939130+SeregaDS@users.noreply.github.com>
2022-05-07 15:53:07 +0000

    Deploying to gh-pages from @ SeregaDS/demo_test@2a0f854b205553a18c61ce97db8b4e83ccbf3553 🚀

--- src.2dc88e5a.js
+++ src.2dc88e5a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/demo_test/src.3887430d.js.map
+//# sourceMappingURL=/demo_test/src.2dc88e5a.js.map
