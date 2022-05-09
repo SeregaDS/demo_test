c167186d3d73ec960dbeb5f9295f0ea0485b3147
Mykhailo Kuzich <timmkedth@gmail.com>
2022-05-09 19:43:46 +0300

    modals close on escape

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,6 +9,12 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  document.addEventListener('keydown', event => event.key === 'Escape'?closeModal():null)
+
+  function closeModal() {
+    document.body.classList.remove('modal-open');
+    refs.modal.classList.add('is-hidden');
+  }
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
